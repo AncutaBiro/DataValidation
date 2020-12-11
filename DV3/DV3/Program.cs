abbf268c9d66c92c34816957dafe88344f9ed01f
AncutaBiro <ancuta.biro@yahoo.com>
2020-12-11 21:49:25 +0200

    solving problem with segment intersection, working on code adjustments

---
+++ DV3/DV3/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace DV3
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine (CheckIntersection (n));
+            Console.Read();
+
+        }
+            static bool CheckIntersection (int n) {
+
+            bool result = false;
+
+            int[] horizontalCoordinates = new int[n + 1];    
+            horizontalCoordinates[0] = 0;
+
+            int[] verticalCoordinates = new int[n + 1];
+            verticalCoordinates[0] = 0;
+
+            for (int k = 1; k < n+1; k++)
+            {
+
+                string direction = Console.ReadLine();
+
+                switch (direction)
+                {
+
+                    case "left":
+
+                        horizontalCoordinates[k] = horizontalCoordinates[k-1] - 1;
+                        verticalCoordinates[k] = verticalCoordinates[k-1] + 0;
+
+
+                        for (int i = 0; i < k - 1; i++)
+                        {
+                            if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
+                            verticalCoordinates[i] == verticalCoordinates[k])
+
+                            {
+                                result = true;
+                            }
+
+                        }
+
+                        break;
+
+                    case "right":
+                        horizontalCoordinates[k] = horizontalCoordinates[k-1] + 1;
+                        verticalCoordinates[k] = verticalCoordinates[k-1] +  0;
+
+                        for (int i = 0; i < k - 1; i++)
+                        {
+                            if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
+                            verticalCoordinates[i] == verticalCoordinates[k])
+
+                            {
+                                result = true;
+                            }
+
+                        }
+                        break;
+
+                    case "up":
+                        verticalCoordinates[k] = verticalCoordinates [k-1] +  1;
+                        horizontalCoordinates[k] = horizontalCoordinates[k-1] +  0;
+
+                        for (int i = 0; i < k - 1; i++)
+                        {
+                            if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
+                            verticalCoordinates[i] == verticalCoordinates[k])
+
+                            {
+                                result = true;
+                            }
+                        }
+
+                        break;
+
+                    case "down":
+                        verticalCoordinates[k] = verticalCoordinates[k-1] - 1;
+                        horizontalCoordinates[k] = horizontalCoordinates[k-1] + 0;
+
+                        for (int i = 0; i < k - 1; i++)
+                        {
+                            if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
+                            verticalCoordinates[i] == verticalCoordinates[k])
+
+                            {
+                                result = true;
+                            }
+                        }
+
+                        break;
+                }
+
+                Console.WriteLine(horizontalCoordinates[k]);
+                Console.WriteLine(verticalCoordinates[k]);
+
+      /*          horizontalCoordinates[k] = x;*/
+
+               /* for (int i = k; i < horizontalCoordinates.Length-1; i++)
+                {
+                        if (horizontalCoordinates[i] == horizontalCoordinates[i - 1]) 
+                    {
+                        result = true;
+                    }
+                }
+
+                verticalCoordinates[k] = y;
+
+                for (int j = k; j < verticalCoordinates.Length-1; j++)
+                        {
+
+                    if (verticalCoordinates[j] == verticalCoordinates[j - 1])
+                    {
+                        result = true;
+                    }
+
+                        }*/
+            
+        }
+
+            return result;
+
+        }
+
+    }
+
+}
+
+
+
+
+   
+
+
---
+++ DataValidation1/DataValidation1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataValidation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+
+            string direction = "up";
+            /*
+                        int[,] location = CheckIntersection(n, direction);
+            */
+     /*       foreach (int i in location)
+            {
+                foreach (int j in location)
+                {*/
+                   CheckIntersection(n, direction);
+
+
+
+
+                }
+
+
+                static string CheckIntersection(int n, string direction)
+
+                {
+            int x = 0;
+            int y = 0;
+
+            List<List<int>> coordonates = new List<List<int>>();
+            
+                 
+                    for (int i = 0; i <= n; i++)
+
+                    {
+
+                List<int> list = new List<int>();
+                coordonates.Add(list);
+
+                switch (direction)
+                        {
+                            case "down":
+                                y--;
+                        coordonates[i].Add(y);
+                                break;
+
+                            case "up":
+                                y++;
+                        coordonates[i].Add(y);
+                        break;
+
+                            case "left":
+                        coordonates[i].Add(x);
+                        x--;
+                                break;
+
+                            case "right":
+                        coordonates[i].Add(x);
+                        x++;
+                                break;
+                        }
+
+                    }
+
+                    return coordonates.ToString();
+                }
+
+
+            }
+        }
+    
