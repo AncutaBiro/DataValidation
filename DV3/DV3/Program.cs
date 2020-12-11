50291ce8ada1aee054d14bb1996d5024b36e5939
AncutaBiro <ancuta.biro@yahoo.com>
2020-12-11 23:41:45 +0200

    code accepted, problem completely solved

--- DV3/DV3/Program.cs
+++ DV3/DV3/Program.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+ Se dă o listă de N tronsoane verticale și orizontale consecutive de dimensiuni fixe sub forma unei succesiuni de direcții (left, right, up, down).
+Scrie o funcție care verifică dacă tronsoanele se intersectează.
+
+Exemplu:
+
+Pentru datele de intrare:
+
+6
+down
+right
+down
+left
+up
+left
+La consolă se va afișa:
+
+True
+ */
+
+using System;
 
 namespace DV3
 {
@@ -7,135 +27,55 @@ namespace DV3
         static void Main()
         {
             int n = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine (CheckIntersection (n));
-            Console.Read();
-
-        }
-            static bool CheckIntersection (int n) {
-
             bool result = false;
 
-            int[] horizontalCoordinates = new int[n + 1];    
+            int[] horizontalCoordinates = new int[n + 1];
             horizontalCoordinates[0] = 0;
 
             int[] verticalCoordinates = new int[n + 1];
             verticalCoordinates[0] = 0;
 
-            for (int k = 1; k < n+1; k++)
+            for (int k = 1; k < n + 1; k++)
             {
-
-                string direction = Console.ReadLine();
-
-                switch (direction)
+                CreateCoordinates(k, horizontalCoordinates, verticalCoordinates);
+                for (int i = 0; i < k - 1; i++)
                 {
+                    if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
+                    verticalCoordinates[i] == verticalCoordinates[k])
+                    {
+                        result = true;
+                    }
+                }
+            }
 
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        static void CreateCoordinates(int k, int[] horizontalCoordinates, int[] verticalCoordinates)
+        {
+                switch (Console.ReadLine())
+                {
                     case "left":
-
-                        horizontalCoordinates[k] = horizontalCoordinates[k-1] - 1;
-                        verticalCoordinates[k] = verticalCoordinates[k-1] + 0;
-
-
-                        for (int i = 0; i < k - 1; i++)
-                        {
-                            if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
-                            verticalCoordinates[i] == verticalCoordinates[k])
-
-                            {
-                                result = true;
-                            }
-
-                        }
-
+                        horizontalCoordinates[k] = horizontalCoordinates[k - 1] - 1;
+                        verticalCoordinates[k] = verticalCoordinates[k - 1] + 0;
                         break;
 
                     case "right":
-                        horizontalCoordinates[k] = horizontalCoordinates[k-1] + 1;
-                        verticalCoordinates[k] = verticalCoordinates[k-1] +  0;
-
-                        for (int i = 0; i < k - 1; i++)
-                        {
-                            if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
-                            verticalCoordinates[i] == verticalCoordinates[k])
-
-                            {
-                                result = true;
-                            }
-
-                        }
+                        horizontalCoordinates[k] = horizontalCoordinates[k - 1] + 1;
+                        verticalCoordinates[k] = verticalCoordinates[k - 1] + 0;
                         break;
 
                     case "up":
-                        verticalCoordinates[k] = verticalCoordinates [k-1] +  1;
-                        horizontalCoordinates[k] = horizontalCoordinates[k-1] +  0;
-
-                        for (int i = 0; i < k - 1; i++)
-                        {
-                            if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
-                            verticalCoordinates[i] == verticalCoordinates[k])
-
-                            {
-                                result = true;
-                            }
-                        }
-
+                        verticalCoordinates[k] = verticalCoordinates[k - 1] + 1;
+                        horizontalCoordinates[k] = horizontalCoordinates[k - 1] + 0;
                         break;
 
                     case "down":
-                        verticalCoordinates[k] = verticalCoordinates[k-1] - 1;
-                        horizontalCoordinates[k] = horizontalCoordinates[k-1] + 0;
-
-                        for (int i = 0; i < k - 1; i++)
-                        {
-                            if (horizontalCoordinates[i] == horizontalCoordinates[k] &&
-                            verticalCoordinates[i] == verticalCoordinates[k])
-
-                            {
-                                result = true;
-                            }
-                        }
-
+                        verticalCoordinates[k] = verticalCoordinates[k - 1] - 1;
+                        horizontalCoordinates[k] = horizontalCoordinates[k - 1] + 0;
                         break;
                 }
-
-                Console.WriteLine(horizontalCoordinates[k]);
-                Console.WriteLine(verticalCoordinates[k]);
-
-      /*          horizontalCoordinates[k] = x;*/
-
-               /* for (int i = k; i < horizontalCoordinates.Length-1; i++)
-                {
-                        if (horizontalCoordinates[i] == horizontalCoordinates[i - 1]) 
-                    {
-                        result = true;
-                    }
-                }
-
-                verticalCoordinates[k] = y;
-
-                for (int j = k; j < verticalCoordinates.Length-1; j++)
-                        {
-
-                    if (verticalCoordinates[j] == verticalCoordinates[j - 1])
-                    {
-                        result = true;
-                    }
-
-                        }*/
-            
+            }
         }
-
-            return result;
-
-        }
-
-    }
-
 }
-
-
-
-
-   
-
-
